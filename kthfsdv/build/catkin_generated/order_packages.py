699d69e1a287606a70b97cfec97f2a4ed6ca697c
AntoineTelecom <antoine.sueur@free.fr>
2018-09-05 20:25:13 +0200

    Added ROS exercise

---
+++ kthfsdv/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ras/kthfsdv/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ras/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ras/catkin_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ kthfsdv/build/exc1/broadcast/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ras/kthfsdv/src/exc1/broadcast
+# Build directory: /home/ras/kthfsdv/build/exc1/broadcast
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
